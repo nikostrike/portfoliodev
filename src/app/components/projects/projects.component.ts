56b768bf60aa8d827c9f7f88b96898b9eab832fd
Nicolas Matias Gonzalez Salgado <nim.gonzalezs@duocuc.cl>
2025-04-24 18:03:26 -0400

    galeria en proyectos

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { TerminalIconComponent } from '../../shared/icons/terminal-icon/terminal-icon.component';
 import { WorkIconComponent } from '../../shared/icons/work-icon/work-icon.component';
+import { PhotoIconComponent } from '../../shared/icons/photo-icon/photo-icon.component';
 
 @Component({
   selector: 'app-projects',
-  imports: [ TerminalIconComponent, WorkIconComponent],
+  imports: [ CommonModule, TerminalIconComponent, WorkIconComponent, PhotoIconComponent, ],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.css'
 })
---
+++ src/app/shared/icons/photo-icon/photo-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-photo-icon',
+  imports: [],
+  templateUrl: './photo-icon.component.html',
+  styleUrl: './photo-icon.component.css'
+})
+export class PhotoIconComponent {
+
+}
